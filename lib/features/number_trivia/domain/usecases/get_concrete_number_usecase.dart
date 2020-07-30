35f97d62008c0f29de3198aa2935eb75db06eb82
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-30 23:57:32 +0100

    refactored the get concrete number method  to be clean

--- lib/features/number_trivia/domain/usecases/get_concrete_number_usecase.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_usecase.dart
@@ -1,16 +1,27 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
 
 import '../../../../core/error/failures.dart';
 import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository numberTriviaRepository;
 
   GetConcreteNumberTrivia(this.numberTriviaRepository);
 
-  Future<Either<Failure, NumberTrivia>> call({@required number}) async {
-    return await numberTriviaRepository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await numberTriviaRepository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number});
+  @override
+  List<Object> get props => [number];
 }
